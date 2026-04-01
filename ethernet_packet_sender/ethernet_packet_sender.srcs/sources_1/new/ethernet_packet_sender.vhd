8276e760549d5a851c34fa28d5a538b907968bde
afflorld <sl.adamec@gmail.com>
2026-04-01 12:45:41 +0200

    project creation

---
+++ ethernet_packet_sender/ethernet_packet_sender.srcs/sources_1/new/ethernet_packet_sender.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/01/2026 12:39:33 PM
+-- Design Name: 
+-- Module Name: ethernet_packet_sender - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ethernet_packet_sender is
+--  Port ( );
+end ethernet_packet_sender;
+
+architecture Behavioral of ethernet_packet_sender is
+
+begin
+
+
+end Behavioral;
