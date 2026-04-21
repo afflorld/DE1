58842b3f1c446df7b16e2fe70ac8b6696bdcff3c
afflorld <sl.adamec@gmail.com>
2026-04-21 23:22:37 +0200

    Top module done

--- vga_graphics_demo/src/img_gen.vhdl
+++ vga_graphics_demo/src/img_gen.vhdl
@@ -1,64 +1,24 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL; 
+use IEEE.NUMERIC_STD.ALL;
 
 entity image_gen is
     Port (
-             clk      : in  STD_LOGIC;
-             video_on : in  STD_LOGIC;
-             x_pos    : in  integer range 0 to 1023; 
-             y_pos    : in  integer range 0 to 511;  
-             btn_u    : in  STD_LOGIC;               
-             btn_d    : in  STD_LOGIC;              
-             btn_r    : in  STD_LOGIC;              
-             rgb      : out STD_LOGIC_VECTOR(11 downto 0)
-         );
+        video_on : in  STD_LOGIC;
+        x_pos    : in  integer range 0 to 1023; 
+        y_pos    : in  integer range 0 to 511;  
+        rect_x   : in  integer range 0 to 639;
+        rect_y   : in  integer range 0 to 479;
+        btn_u    : in  STD_LOGIC;               
+        rgb      : out STD_LOGIC_VECTOR(11 downto 0)
+    );
 end image_gen;
 
 architecture Behavioral of image_gen is
-
-
-    constant SQ_SIZE : integer := 64;
-    constant X_MAX   : integer := 640;
-    constant SQ_Y    : integer := 208; 
-
-
-    signal sq_x  : integer range 0 to 1023 := (X_MAX - SQ_SIZE) / 2;
-    signal dir_x : std_logic := '1';     
-
-    signal frame_tick : std_logic;
-
+    constant SQ_SIZE : integer := 50;
 begin
 
-
-    frame_tick <= '1' when (x_pos = 0 and y_pos = 0) else '0';
-
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            if btn_r = '1' then
-                sq_x  <= (X_MAX - SQ_SIZE) / 2;
-                dir_x <= '1';
-
-            elsif btn_d = '1' and frame_tick = '1' then
-                if dir_x = '1' then
-                    if sq_x >= (X_MAX - SQ_SIZE - 2) then
-                        dir_x <= '0';
-                    else
-                        sq_x <= sq_x + 2; 
-                    end if;
-                else
-                    if sq_x <= 2 then
-                        dir_x <= '1';
-                    else
-                        sq_x <= sq_x - 2;
-                    end if;
-                end if;
-            end if;
-        end if;
-    end process;
-
-    process(x_pos, y_pos, video_on, btn_u, sq_x)
+    process(x_pos, y_pos, video_on, btn_u, rect_x, rect_y)
         variable bit_x, bit_y, checker : std_logic;
     begin
         bit_x := to_unsigned(x_pos, 10)(5);
@@ -69,22 +29,18 @@ begin
             rgb <= x"000"; 
 
         elsif btn_u = '1' then
-
             if checker = '1' then
                 rgb <= x"FFF"; 
             else
                 rgb <= x"000"; 
             end if;
 
-        elsif (x_pos >= sq_x) and (x_pos < sq_x + SQ_SIZE) and 
-        (y_pos >= SQ_Y) and (y_pos < SQ_Y + SQ_SIZE) then
-
-            rgb <= x"0F0"; 
+        elsif (x_pos >= rect_x) and (x_pos < rect_x + SQ_SIZE) and 
+              (y_pos >= rect_y) and (y_pos < rect_y + SQ_SIZE) then
+            rgb <= x"0F0";
 
         else
-
-            rgb <= x"000"; 
+            rgb <= x"000";
         end if;
     end process;
-
 end Behavioral;
--- vga_graphics_demo/src/vga_top.vhdl
+++ vga_graphics_demo/src/vga_top.vhdl
@@ -1,43 +1,157 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/21/2026 10:09:27 PM
--- Design Name: 
--- Module Name: image_gen - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity vga_top is
---  Port ( );
+    port ( 
+             clk       : in  std_logic;
+             btnc      : in  std_logic;
+             btnu      : in  std_logic;
+             btnd      : in  std_logic;
+             btnl      : in  std_logic;
+             btnr      : in  std_logic;
+
+             vga_xsync : out std_logic;        
+             vga_ysync : out std_logic;        
+             vga_r     : out std_logic_vector (3 downto 0);
+             vga_g     : out std_logic_vector (3 downto 0);
+             vga_b     : out std_logic_vector (3 downto 0)
+         );
 end vga_top;
 
-architecture Behavioral of vga_top is
+architecture behavioral of vga_top is
+
+    component clk_en is
+        generic ( g_max : positive );
+        port ( clk, rst : in std_logic; ce : out std_logic );
+    end component;
+
+    component debounce is
+        port ( clk, rst, btn_in : in std_logic; btn_state : out std_logic );
+    end component;
+
+    component vga_sync is
+        port (
+        clk, rst, en_25 : in std_logic;
+        x_sync, y_sync, video_on : out std_logic;
+        x_pos : out integer range 0 to 1023;
+        y_pos : out integer range 0 to 511
+    );
+    end component;
+
+    component rect_movement_fsm is
+        port (
+        clk, reset, vsync : in std_logic;
+        btn_up, btn_down, btn_left, btn_right : in std_logic;
+        rect_x : out integer range 0 to 639;
+        rect_y : out integer range 0 to 479
+    );
+    end component;
+
+    component image_gen is
+        port (
+                 video_on : in std_logic;
+                 x_pos    : in integer range 0 to 1023;
+                 y_pos    : in integer range 0 to 511;
+                 rect_x   : in integer range 0 to 639;
+                 rect_y   : in integer range 0 to 479;
+                 btn_u    : in std_logic;
+                 rgb      : out std_logic_vector(11 downto 0)
+             );
+    end component;
+
+    signal sig_en_25    : std_logic;
+    signal sig_ysync    : std_logic;
+    signal sig_video_on : std_logic;
+
+    signal sig_x_pos    : integer range 0 to 1023;
+    signal sig_y_pos    : integer range 0 to 511;
+
+    signal sig_rect_x   : integer range 0 to 639;
+    signal sig_rect_y   : integer range 0 to 479;
+
+    signal sig_btnu, sig_btnd, sig_btnl, sig_btnr : std_logic;
+
+    signal sig_rgb_full : std_logic_vector(11 downto 0);
 
 begin
 
+    ce_gen_inst : clk_en
+    generic map ( g_max => 4 )
+    port map (
+                 clk => clk,
+                 rst => btnc,
+                 ce  => sig_en_25
+             );
 
-end Behavioral;
+    deb_u : debounce 
+    port map (
+                 clk => clk,
+                 rst => btnc,
+                 btn_in => btnu,
+                 btn_state => sig_btnu
+             );
+    deb_d : debounce 
+    port map (
+                 clk => clk,
+                 rst => btnc,
+                 btn_in => btnd,
+                 btn_state => sig_btnd
+             );
+    deb_l : debounce 
+    port map (
+                 clk => clk,
+                 rst => btnc,
+                 btn_in => btnl,
+                 btn_state => sig_btnl
+             );
+    deb_r : debounce 
+    port map (
+                 clk => clk,
+                 rst => btnc,
+                 btn_in => btnr,
+                 btn_state => sig_btnr
+             );
+
+    vga_timing : vga_sync
+    port map (
+                 clk      => clk,
+                 rst      => btnc,
+                 en_25    => sig_en_25,
+                 x_sync   => vga_xsync,
+                 y_sync   => sig_ysync,
+                 video_on => sig_video_on,
+                 x_pos    => sig_x_pos,
+                 y_pos    => sig_y_pos
+             );
+
+    vga_ysync <= sig_ysync;
+
+    move_logic : rect_movement_fsm
+    port map (
+                 clk       => clk,
+                 reset     => btnc,
+                 vsync     => sig_ysync,
+                 btn_up    => '0',
+                 btn_down  => sig_btnd,
+                 btn_left  => sig_btnl,
+                 btn_right => sig_btnr,
+                 rect_x    => sig_rect_x,
+                 rect_y    => sig_rect_y
+             );
+
+    pixel_gen : image_gen
+    port map (
+                 video_on => sig_video_on,
+                 x_pos    => sig_x_pos,
+                 y_pos    => sig_y_pos,
+                 rect_x   => sig_rect_x,
+                 rect_y   => sig_rect_y,
+                 btn_u    => sig_btnu,
+                 rgb      => sig_rgb_full
+             );
+
+    vga_r <= sig_rgb_full(11 downto 8);
+    vga_g <= sig_rgb_full(7 downto 4);
+    vga_b <= sig_rgb_full(3 downto 0);
+
+end behavioral;
